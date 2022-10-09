4552c3b3b07f5a7b6850450d65916049ea3e262f
Netanel86 <iting86@gmail.com>
2022-10-10 00:03:33 +0300

    implemented a hierarchical representation of data-bases constants in a new module

---
+++ server/constants.py
@@ -0,0 +1,131 @@
+""" A hierarchical view of local and remote data base's constants.
+"""
+from __future__ import annotations
+from collections import namedtuple
+from typing import Any, NamedTuple, Tuple
+from sqlite import TYPES
+
+# region Remote constants
+__RemCollections = namedtuple("__RemColls", "SYSTEMS MODULES COMMANDS SENSORS")
+__REM_COLLS = __RemCollections("systems", "modules", "commands", "sensors")
+
+__ModuleRemFields = namedtuple(
+    "__ModuleRemFields", "ID IP DESCRIPTION MAX_DURATION DURATION ON_TIME"
+)
+__REM_MOD_FIELDS = __ModuleRemFields(
+    "id", "ip", "description", "maxDuration", "duration", "onTime"
+)
+
+__SystemRemFields = namedtuple("__SystemRemFields", "ID")
+__REM_SYS_FIELDS = __SystemRemFields("id")
+
+__CommRemFields = namedtuple("__CommRemFields", "ID ACTION TIME ATTR")
+__REM_COMM_FIELDS = __CommRemFields("id", "action", "timestamp", "attributes")
+
+
+class __ModuleRem(NamedTuple):
+    FieldName: __ModuleRemFields
+    FIELDS: Tuple[str]
+    COLL_NAME: str
+
+
+__REM_MODULE = __ModuleRem(
+    __REM_MOD_FIELDS, tuple(__REM_MOD_FIELDS), __REM_COLLS.MODULES
+)
+
+
+class __CommandRem(NamedTuple):
+    FieldName: __CommRemFields
+    COLL_NAME: str
+
+
+__REM_COMM = __CommandRem(__REM_COMM_FIELDS, __REM_COLLS.COMMANDS)
+
+
+class __SystemRem(NamedTuple):
+    FieldName: __SystemRemFields
+    COLL_NAME: str
+
+
+__REM_SYS = __SystemRem(__REM_SYS_FIELDS, __REM_COLLS.SYSTEMS)
+
+
+class __SensorRem(NamedTuple):
+    COLL_NAME: str
+
+
+__REM_SENS = __SensorRem(__REM_COLLS.SENSORS)
+
+
+class __Remote(NamedTuple):
+    Commands: __CommandRem
+    Modules: __ModuleRem
+    Sensors: __SensorRem
+    Systems: __SystemRem
+
+
+Remote = __Remote(__REM_COMM, __REM_MODULE, __REM_SENS, __REM_SYS)
+""" A hierarchical view on remote data base's collections and field names.
+"""
+# endregion
+
+# region Local constants
+__LocTables = namedtuple("__LocTableNames", "SYSTEM MODULES SENSORS")
+__LOC_TABLES = __LocTables("system", "modules", "sensors")
+
+__ModuleLocColumns = namedtuple(
+    "__ModuleLocColumns", "ID IP DESCRIPTION MAX_DURATION DURATION ON_TIME PORT TIMEOUT"
+)
+__LOC_MOD_COLUMNS = __ModuleLocColumns(
+    "id", "ip", "description", "max_duration", "duration", "on_time", "port", "timeout"
+)
+__LOC_MOD_TYPES = (
+    (__LOC_MOD_COLUMNS.ID, TYPES.TEXT),
+    (__LOC_MOD_COLUMNS.IP, TYPES.TEXT),
+    (__LOC_MOD_COLUMNS.DESCRIPTION, TYPES.TEXT),
+    (__LOC_MOD_COLUMNS.MAX_DURATION, TYPES.INT),
+    (__LOC_MOD_COLUMNS.DURATION, TYPES.INT),
+    (__LOC_MOD_COLUMNS.ON_TIME, TYPES.TIME),
+    (__LOC_MOD_COLUMNS.PORT, TYPES.INT),
+    (__LOC_MOD_COLUMNS.TIMEOUT, TYPES.FLOAT),
+)
+
+__SystemLocColumns = namedtuple("__SystemLocColumns", "ID")
+__LOC_SYS_COLUMNS = __SystemLocColumns("id")
+__LOC_SYS_TYPES = ((__LOC_SYS_COLUMNS.ID, TYPES.TEXT),)
+
+
+class __ModuleLoc(NamedTuple):
+    ColName: __ModuleLocColumns
+    COLUMNS: Tuple
+    TABLE_NAME: str
+    TYPE_MAP: Tuple[Tuple]
+
+
+__LOC_MOD = __ModuleLoc(
+    __LOC_MOD_COLUMNS,
+    tuple(__LOC_MOD_COLUMNS),
+    __LOC_TABLES.MODULES,
+    __LOC_MOD_TYPES,
+)
+
+
+class __SystemLoc(NamedTuple):
+    ColName: __SystemLocColumns
+    COLUMNS: Tuple
+    TABLE_NAME: str
+    TYPE_MAP: Tuple[Tuple]
+
+
+__LOC_SYS = __SystemLoc(
+    __LOC_SYS_COLUMNS, tuple(__LOC_SYS_COLUMNS), __LOC_TABLES.SYSTEM, __LOC_SYS_TYPES
+)
+
+
+class __Local(NamedTuple):
+    Modules: __ModuleLoc
+    System: __SystemLoc
+
+
+Local = __Local(__LOC_MOD, __LOC_SYS)
+# endregion
