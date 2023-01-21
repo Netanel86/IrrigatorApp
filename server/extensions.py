09d70532812a787a99d0b640be4bc541c4063aa3
Netanel86 <iting86@gmail.com>
2023-01-22 01:48:07 +0200

    added documentation

--- server/extensions.py
+++ server/extensions.py
@@ -1,9 +1,13 @@
-from typing import Any, Callable, Dict, Generic, Iterable, TypeVar
+from typing import Dict, Iterable
 
 
 def reverse_dict(dict: Dict):
+    """Reverese dictionary `(key: value)` pairs to `(value: key)` pairs"""
     return {val: key for key, val in dict.items()}
 
 
 def is_empty(iterable: Iterable):
+    """Returns `True` if the iterable collection is empty."""
     return len(iterable) == 0
+
+
